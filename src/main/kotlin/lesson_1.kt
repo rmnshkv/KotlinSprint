49b92bdf4b994e09f333cda6349ca0d8564dd053
rmnshkv <r.shikov@yandex.ru>
2023-09-04 15:45:36 +0300

    KS-1-1 print data from first lesson

---
+++ src/main/kotlin/lesson_1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    //println("Hello World!")
+
+    val numberOfOrders = 75
+
+    val notificationText = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(notificationText)
+}

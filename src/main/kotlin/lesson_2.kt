426afdc2b0127b651f6779714258b05722682cda
rmnshkv <r.shikov@yandex.ru>
2023-09-05 00:29:44 +0300

    KS-1-2 second lesson

---
+++ src/main/kotlin/lesson_2.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val numberOfEmployees = 2000
+
+    val notificationText = "Количество сотрудников"
+
+    //println(numberOfEmployees)
+    println(notificationText)
+
+    println (numberOfEmployees - 1)
+}

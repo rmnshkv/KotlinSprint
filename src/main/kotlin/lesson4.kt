97450034c49babf4f4feb8fafafcf1a5604d0b6b
rmnshkv <r.shikov@yandex.ru>
2023-09-08 19:00:22 +0300

    KS-1-4 data types

---
+++ src/main/kotlin/lesson4.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+val length: Long = 40_868_600_000
+val age: Byte = 27
+val partOfDay: Double = 0.075
+val seconds: Short = 6480
+val partOfYear: Float = 2.0547945205479453E-4f
+val apogee: Int = 327000
+
+}

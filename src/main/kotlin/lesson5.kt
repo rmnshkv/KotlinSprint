d6124db08876b48e3fc96876d98d0262d5d5eaff
rmnshkv <r.shikov@yandex.ru>
2023-09-10 20:44:46 +0300

    KS-1-5 hours

--- src/main/kotlin/lesson5.kt
+++ src/main/kotlin/lesson5.kt
@@ -1,15 +1,10 @@
 fun main() {
-
     val seconds = 6480
-    val minutes = (seconds / 60)
-    val hours = (minutes / 60f)
-    println(minutes)
-    println(hours)
 
-    // 108 минут (1.8 часа) = 1 час 48 минут 0 секунд
-    val hour = "01"
-    val min = "48"
-    val sec = "00"
+    val hour = seconds / 3600
+    val minute = (seconds % 3600) / 60
+    val second = seconds % 60
 
-    println("$hour:$min:$sec")
+    println (String.format("%02d:%02d:%02d", hour, minute, second))
+
 }

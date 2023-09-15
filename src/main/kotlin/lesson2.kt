b9c1b55dd7be6e77a7dcdc39f00fd20a9c5e0cc0
rmnshkv <r.shikov@yandex.ru>
2023-09-15 11:50:20 +0300

    KS-2-2 Arithmetic lesson 2 fixed

---
+++ src/main/kotlin/lesson2.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val staff = 50
+    val stsalary = 30000
+    val jun = 30
+    val junsalary = 20000
+
+    println (staff * stsalary)
+    val all_staff_salary = 1500000
+    println (all_staff_salary + (jun * junsalary))
+    val total_payment = 2100000
+    println (total_payment / (staff + jun))
+    val arithmetic_mean = 26250
+}

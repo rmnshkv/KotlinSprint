b50548fa27960a38fdf42f841c52b58d06ca72ec
rmnshkv <r.shikov@yandex.ru>
2023-09-20 17:47:27 +0300

    KS-2-3 Arithmetic lesson 3 fixed

--- src/main/kotlin/lesson3.kt
+++ src/main/kotlin/lesson3.kt
@@ -1,10 +1,10 @@
 fun main() {
-    val DepartureHours = 9
-    val DepartureMinutes = 39
+    val departureHours = 9
+    val departureMinutes = 39
 
     val travelTime = 457
 
-    val arrivalTime = (DepartureHours * 60 + DepartureMinutes) + travelTime
+    val arrivalTime = (departureHours * 60 + departureMinutes) + travelTime
     val arrivalHours = arrivalTime / 60
     val arrivalMinutes = arrivalTime % 60
 

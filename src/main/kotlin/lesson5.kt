2d451316c028a0c8314adc34597876f513bb49c6
rmnshkv <r.shikov@yandex.ru>
2023-09-26 16:32:31 +0300

    KS-3-5 pars chess

---
+++ src/main/kotlin/lesson5.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val string = "D2-D4;0"
+    val parts = string.split("[\\-\\;]".toRegex())
+
+    val firstMove = parts[0]
+    println(firstMove)
+    val secondMove = parts[1]
+    println(secondMove)
+    val numberOfMove = parts[2]
+    println(numberOfMove)
+
+}

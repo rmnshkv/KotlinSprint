bdacfc2864b6a5ca7f40c7550767119b12d798d2
rmnshkv <r.shikov@yandex.ru>
2023-10-10 17:05:51 +0300

    KS-4-1 Comparison operators FIX

--- src/main/kotlin/lesson1.kt
+++ src/main/kotlin/lesson1.kt
@@ -1,10 +1,12 @@
 fun main() {
- val totalTables = 13
+
  val occupiedToday = 13
  val occupiedTomorrow = 9
- val resultToday = occupiedToday < ACESSABLE_TABLE
- val resultTomorrow = occupiedTomorrow >= ACESSABLE_TABLE
+
+ val resultToday = occupiedToday < TOTAL_TABLES
+ val resultTomorrow = occupiedTomorrow < TOTAL_TABLES
+
  println("Доступность столиков на сегодня: $resultToday" +
          "\nДоступность столиков на завтра: $resultTomorrow")
 }
-const val ACESSABLE_TABLE = 1
+const val TOTAL_TABLES = 13

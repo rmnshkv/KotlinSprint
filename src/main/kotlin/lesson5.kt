9484ee7b4623f60fe54d3b0a551b7a52fdc828aa
rmnshkv <r.shikov@yandex.ru>
2023-10-11 17:18:16 +0300

    KS-4-5 can ship sail console fix

--- src/main/kotlin/lesson5.kt
+++ src/main/kotlin/lesson5.kt
@@ -1,20 +1,18 @@
 fun canShipSail(damaged: Boolean, crew: Int, provisions: Int, favorableWeather: Boolean): Boolean {
-    if (!damaged) {
-        if (crew in 55..70 && provisions > 50) {
+        if (!damaged && crew in 55..70 && provisions > 50) {
             return true
-        } else if (!damaged && crew == 70 && provisions >= 50 && favorableWeather) {
+        } else if (crew == 70 && provisions >= 50 && favorableWeather) {
             return true
         }
-    }
     return false
 }
 
 fun main() {
-    val damaged = readLine()?.equals("да", ignoreCase = true) ?: false
+    val damaged = readln().equals("да", ignoreCase = true) ?: false
     print("Введите текущий состав экипажа: ")
     val crew = readLine()?.toIntOrNull() ?: 0
     print("Введите количество ящиков с провизией на борту: ")
-    val provisions = readLine()?.toIntOrNull() ?: 0
+    val provisions = readln().toIntOrNull() ?: 0
     val favorableWeather = readLine()?.equals("да", ignoreCase = true) ?: false
 
     if (canShipSail(damaged, crew, provisions, favorableWeather)) {

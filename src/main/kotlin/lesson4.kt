9130e8ea073e9034246275900ae1326de6aed4c7
rmnshkv <r.shikov@yandex.ru>
2023-10-13 17:07:52 +0300

    KS-4-4 training app

--- src/main/kotlin/lesson4.kt
+++ src/main/kotlin/lesson4.kt
@@ -1,14 +1,11 @@
 fun main() {
-
     val day = 5
-    val arms= day % 2 == 1
-    val legs = day % 2 != 1
-    val back = day % 2 != 1
-    val abs = day % 2 == 1
+    val isEven = day % 2 == 0
 
-    println("""Упражнения для рук: $arms
-    "Упражнения для ног: $legs
-    Упражнения для спины: $back
-    Упражнения для пресса: $abs
+    println("""Упражнения для рук: ${!isEven}
+    "Упражнения для ног: ${isEven}
+    Упражнения для спины: ${isEven}
+    Упражнения для пресса: ${!isEven}
     """)
+
 }

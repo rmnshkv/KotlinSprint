fd32e6e51bbd815012891710a4deb161c019c000
rmnshkv <r.shikov@yandex.ru>
2023-10-17 11:16:02 +0300

    KS-4-3 bean conditions fix2

--- src/main/kotlin/lesson3.kt
+++ src/main/kotlin/lesson3.kt
@@ -5,11 +5,11 @@ fun main() {
     val airHumidity = 20
     val season = "winter"
 
-    val isGoodConditions = isSunny == IS_SUNNY && isAwningOpen == IS_AWNING_OPEN && airHumidity == AIR_HUMIDITY && season == SEASON
+    val isGoodConditions = isSunny == IS_SUNNY && isAwningOpen == IS_AWNING_OPEN && airHumidity == AIR_HUMIDITY && season != SEASON
 
     println("Благоприятные ли сейчас условия для роста бобовых? $isGoodConditions")
 }
 const val IS_SUNNY = true
 const val IS_AWNING_OPEN = true
 const val AIR_HUMIDITY = 20
-const val SEASON = "not winter"
+const val SEASON = "winter"

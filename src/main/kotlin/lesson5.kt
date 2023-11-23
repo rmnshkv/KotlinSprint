f84640a7ea7170d1118b739a7ef0b84b986e352c
rmnshkv <r.shikov@yandex.ru>
2023-11-23 18:29:09 +0300

    KS-4-5 can ship sail console fix

--- src/main/kotlin/lesson5.kt
+++ src/main/kotlin/lesson5.kt
@@ -1,23 +1,30 @@
-fun canShipSail(damaged: Boolean, crew: Int, provisions: Int, favorableWeather: Boolean): Boolean {
-        if (!damaged && crew in 55..70 && provisions > 50) {
+import kotlin.math.min
+
+fun canShipSail(isDamaged: Boolean, crew: Int, provisions: Int, favorableWeather: Boolean): Boolean {
+        if (!isDamaged && crew >= CREW_MIN && crew <= CREW_MAX && provisions > PROVISIONS) {
             return true
-        } else if (crew == 70 && provisions >= 50 && favorableWeather) {
+        } else if (crew == CREW_MAX && provisions >= PROVISIONS && favorableWeather) {
             return true
         }
     return false
 }
 
 fun main() {
-    val damaged = readln().equals("да", ignoreCase = true) ?: false
+    print("Корабль поврежден? да или нет: ")
+    val isDamaged = readln().equals("да", ignoreCase = true) ?: false
     print("Введите текущий состав экипажа: ")
     val crew = readLine()?.toIntOrNull() ?: 0
     print("Введите количество ящиков с провизией на борту: ")
     val provisions = readln().toIntOrNull() ?: 0
+    print("Багоприятна ли погода? да или нет: ")
     val favorableWeather = readLine()?.equals("да", ignoreCase = true) ?: false
 
-    if (canShipSail(damaged, crew, provisions, favorableWeather)) {
+    if (canShipSail(isDamaged, crew, provisions, favorableWeather)) {
         println("Корабль может отправиться в плавание.")
     } else {
         println("Корабль не может отправиться в плавание.")
     }
 }
+const val CREW_MIN = 55
+const val CREW_MAX = 70
+const val PROVISIONS = 50
